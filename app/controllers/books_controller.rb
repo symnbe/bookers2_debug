34a52bdf208ed23766694f1bbc00bba3ef287c4c
symnbe <ookaminingen@me.com>
2023-03-01 23:52:43 +0000

    [debug処理]Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,15 @@
 class BooksController < ApplicationController
 
-  def show
-    @book = Book.find(params[:id])
+  def index
+    @book =  Book.new
+    @books = Book.all
+    @user = current_user
   end
 
-  def index
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
     @books = Book.all
   end
 
@@ -15,12 +20,18 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      @user = current_user
+      render :index
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+          render "edit"
+      else
+          redirect_to books_path
+      end
   end
 
   def update
@@ -28,19 +39,19 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
