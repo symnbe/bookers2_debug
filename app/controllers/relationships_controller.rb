b36e643a19d50f0e4b9d41a3e79460e78dcb3144
symnbe <ookaminingen@me.com>
2023-03-05 07:23:21 +0000

    [Add_follow機能]user/index_page

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,24 @@
+class RelationshipsController < ApplicationController
+
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+    # redirect_back fallback_location: root_path
+  end
+
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/followings/:user_id' => 'relationships#followings',as: 'followings'
+  get 'relationships/followers/:user_id' => 'relationships#followers', as: 'followers'
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
@@ -6,5 +8,10 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    # get 'followings' => 'relationships#followings', as: 'followings'
+    # get 'followers' => 'relationships#followers', as: 'followers'
+  end
 end
