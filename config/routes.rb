043ae04dd29c578bf86dbce712db78476496f85d
symnbe <ookaminingen@me.com>
2023-03-06 07:53:10 +0000

    [ADD]検索機能に伴うroutesの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'relationships/followings/:user_id' => 'relationships#followings',as: 'followings'
-  get 'relationships/followers/:user_id' => 'relationships#followers', as: 'followers'
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get 'relationships/followings/:user_id' => 'relationships#followings',as: 'followings'
+  get 'relationships/followers/:user_id' => 'relationships#followers', as: 'followers'
+  get 'search' => "searches#search"
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    # get 'followings' => 'relationships#followings', as: 'followings'
-    # get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
+  
 end
