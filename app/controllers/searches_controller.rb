3f0d377fc3fa7c8aaa0e9d7eecd7591a9eccf03d
symnbe <ookaminingen@me.com>
2023-03-07 10:37:27 +0000

    [add]検索機能_html

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    @range = params([:range])
+    @range = params[:range]
 
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,14 +2,14 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?", "#{word}")
@@ -21,5 +21,6 @@ class Book < ApplicationRecord
       @book = Book.where("title LIKE?", "%#{word}%")
     else
       @book = Book.all
+    end
   end
 end
