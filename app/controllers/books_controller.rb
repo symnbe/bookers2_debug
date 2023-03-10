e738186d0cb60e59c50f38c4c1824219b7f1be90
symnbe <ookaminingen@me.com>
2023-03-10 05:32:42 +0000

    [Add]Books/index/ranking

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,20 @@
 class BooksController < ApplicationController
 
   def index
+    # @week_book_favorite_ranks = Book.find(Favorite.group(:book_id).where(created_at: Time.current.all_week).order('count(book_id) desc').pluck(:book_id))
+    # period = Time.current.all_week
+    # @books = Book.includes(:favorited_users).
+    #   sort_by {|x|
+    #   x.favorited_users.includes(:favorites).where(created_at: period).size
+    #   }.reverse
+
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
     @book =  Book.new
-    @books = Book.all
     @user = current_user
   end
 
