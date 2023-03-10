6f8f86b32e9e18a1fdc207e3073d0a6f7bd793de
symnbe <ookaminingen@me.com>
2023-03-10 05:36:37 +0000

    [Remove]books/index/ranking

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,9 @@ class BooksController < ApplicationController
     #   x.favorited_users.includes(:favorites).where(created_at: period).size
     #   }.reverse
 
-    to = Time.current.at_end_of_day
-    from = (to - 6.day).at_beginning_of_day
-    @books = Book.includes(:favorited_users).
-      sort_by {|x|
-        x.favorited_users.includes(:favorites).where(created_at: from...to).size
-      }.reverse
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).sort_by {|x| x.favorites.where(created_at: from...to).size}.reverse
     @book =  Book.new
     @user = current_user
   end
