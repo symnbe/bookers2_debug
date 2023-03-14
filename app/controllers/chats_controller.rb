a3e13bc1407eb2fc904b88e093432d459eb347f5
symnbe <ookaminingen@me.com>
2023-03-14 00:33:37 +0000

    [Add]DM機能

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,6 @@
-class ChatsController < ApplicationController
-before_action :reject_non_related, only: [:show]
 
+class ChatsController < ApplicationController
+  before_action :reject_non_related, only: [:show]
   def show
     @user = User.find(params[:id])
     rooms = current_user.user_rooms.pluck(:room_id)
@@ -16,23 +16,21 @@ before_action :reject_non_related, only: [:show]
     end
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
+  end
+  def create
+    @chat = current_user.chats.new(chat_params)
+    render :validater unless @chat.save
+  end
 
-    def create
-      @chat = current_user.chats.new(chat_params)
-      render :validater unless @chat.save
+  private
+  def chat_params
+    params.require(:chat).permit(:message, :room_id)
+  end
+
+  def reject_non_related
+    user = User.find(params[:id])
+    unless current_user.following?(user) && user.following?(current_user)
+      redirect_to books_path
     end
-
-    private
-
-    def chat_params
-      params.require(:chat).permit(:message, :room_id)
-    end
-
-    def reject_non_related
-      user = User.find(params[:id])
-
-      unless current_user.following?(user) && user.following?(current_user)
-        redirect_to books_path
-      end
-    end
+  end
 end
